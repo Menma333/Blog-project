4f792d75ebff4b41bb52862df9cae4b8f2b7cf6c
Shvager Artur <menma0n@gmail.com>
2023-07-22 13:37:06 +0200

    connect bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,6 @@ import Slider from "./components/Slider/Slider.tsx";
 
 
 function App() {
-
-  const items = [
-    {
-      title: 'React',
-      text: 'React is an element of programming. A specialized technology used by JavaScript in the development of a variety of content. It is a library for creating user interfaces. It is open source.',
-      img: 'React.png',
-    },
-    {
-      title: 'Angular',
-      text: 'Angular is a framework from Google for creating advanced seamless (single page) web applications - SPA (Single Page Applications) - in the programming languages ​​TypeScript, JavaScript, Dart. The framework is open source. The product is distributed free of charge.',
-      img: 'Angular.jpeg',
-    },
-    {
-      title: 'Vue',
-      text: 'Vue. js is a progressive framework for developing user interfaces and single-page web applications in JavaScript. It solves the tasks of the presentation layer (view) and simplifies the work with libraries. Vue.',
-      img: 'Vue.png',
-    },
-  ]
-
   return (
     <div className="App">
       <Header />
--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,18 +1,10 @@
 import React, { Component } from "react";
-import { useState, useEffect, useRef } from "react";
 import "../Slider/Slider.css";
-import ReactImg from "../../img/React.png";
-import AngularImg from "../../img/Angular.jpeg";
-import VueImg from "../../img/Vue.png";
 
-const Slider = (props) => {
-  return (
-    <div className={props.slider}>
-      <img src={ReactImg} alt="React" className="slider__img" />
-      <img src={AngularImg} alt="React" className="slider__img" />
-      <img src={VueImg} alt="React" className="slider__img" />
-    </div>
-  );
-};
+export class Slider extends Component {
+  render() {
+    return <div>Slider</div>;
+  }
+}
 
 export default Slider;
