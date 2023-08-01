28985ad344079c6b61347ca40d44d779a224e2bf
Shvager Artur <menma0n@gmail.com>
2023-08-01 22:56:36 +0200

    fix

--- src/components/ArticleItem/ArticleItem.jsx
+++ src/components/ArticleItem/ArticleItem.jsx
@@ -5,11 +5,16 @@ import "./ArticleItem.css";
 const ArticleItem = ({ article, handleLikeClicked }) => {
   const [isLiked, setIsLiked] = useState(false);
 
+  const handleLikeButtonClicked = () => {
+    handleLikeClicked(article);
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div className="article">
       <h2 className="article-title">{article.title}</h2>
       <p className="article-content">{article.content}</p>
-      <button className="like__button" onClick={handleLikeClicked}>
+      <button className="like__button" onClick={handleLikeButtonClicked}>
         {isLiked ? <BsFillHeartFill color="red" /> : <BsFillHeartFill />}
       </button>
     </div>
