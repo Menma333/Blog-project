5232720f169aafe9bcf864f177b0659fdd803670
Shvager Artur <menma0n@gmail.com>
2023-08-02 12:29:23 +0200

    Removed unnecessary imports and corrected the transfer of text to buttons in the ShowFullItem category

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Blog from "./pages/Blog/Blog";
